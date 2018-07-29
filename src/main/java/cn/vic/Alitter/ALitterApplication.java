9dca1f23a16b848152f3cf30e6d43769d0f29903
yesixuan <18373209286@163.com>
2018-07-29 22:51:35 +0800

    初始化项目，反向生成mybatis配置

---
+++ src/main/java/cn/vic/Alitter/ALitterApplication.java
@@ -0,0 +1,12 @@
+package cn.vic.Alitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ALitterApplication.class, args);
+    }
+}
